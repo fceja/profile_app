7782e75bc8a7abffc05198c49945ff808154ed5d
fceja <cisco_ceja@icloud.com>
2023-07-27 13:36:37 -0700

    add hero component

---
+++ my-app/src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SetScrollVar from "../scripts/HeroScript";
+
+import "../styles/Hero.css";
+
+export default function Hero() {
+  SetScrollVar();
+
+  return <div>Hi from Hero Component</div>;
+}
