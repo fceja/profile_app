152004bd379750295ad361eccef72fbe3f6a0b70
fceja <cisco_ceja@icloud.com>
2023-07-28 10:15:00 -0700

    add Header component

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css"
+
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="headerContainer">
+            <div className="headerDiv">This is header div</div>
+        </header>
+    )
+}
