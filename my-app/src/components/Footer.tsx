822dfdabd5a81d28096210ab4133e12e084d2b29
fceja <cisco_ceja@icloud.com>
2023-07-31 10:38:12 -0700

    add footer component

---
+++ my-app/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "../styles/Footer.css";
+
+import React from "react";
+
+export default function Footer() {
+  const currentDate = new Date();
+  const year = currentDate.toLocaleDateString(undefined, { year: "numeric" });
+
+  return <footer>&copy; {year}, AB Company</footer>;
+}
