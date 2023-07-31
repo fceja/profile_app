035a69e7ff338f84ddb8713f68c163e044b5a995
FC <cisco_ceja@icloud.com>
2023-07-31 15:24:53 -0700

    update CTAlink text

--- my-app/src/components/CTALinks.tsx
+++ my-app/src/components/CTALinks.tsx
@@ -5,9 +5,9 @@ import React from "react";
 export default function CTALinks() {
   return (
     <div className='CTA-link-container'>
-      <a className="CTA" href="http://localhost:3000/">Link1</a>
-      <a className="CTA" href="http://localhost:3000/">Link2</a>
-      <a className="CTA" href="http://localhost:3000/">Link3</a>
+      <a className="CTA" href="http://localhost:3000/">Gallery</a>
+      <a className="CTA" href="http://localhost:3000/">About</a>
+      <a className="CTA" href="http://localhost:3000/">Contact</a>
     </div>
   );
 }
