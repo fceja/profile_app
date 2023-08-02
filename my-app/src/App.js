710c9dba2efdcd38b4fad4d7f6974ea0f754e54c
fceja <cisco_ceja@icloud.com>
2023-08-02 08:19:06 -0700

    add components into template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,11 @@
 import "./styles/App.css";
 
-import Footer from "./components/Footer.tsx";
-import Logo from "./components/Logo";
-import NavBar from "./components/NavBar.tsx";
-import Pages from "./components/Pages";
+import LandingPageTemplateV1 from "./templates/LandingPageTemplateV1";
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <NavBar />
-      <Pages />
-      <Footer />
+      <LandingPageTemplateV1 />
     </div>
   );
 }
