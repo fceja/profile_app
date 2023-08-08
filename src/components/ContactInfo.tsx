3f318bd884c0457df3bb95f925a23b70c67e99db
FC <cisco_ceja@icloud.com>
2023-08-08 08:37:53 -0700

    add base work for contact form

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ContactInfo() {
+    return(
+        <div className="contact-info">
+            This is Contact Info Container
+        </div>
+    )
+}
