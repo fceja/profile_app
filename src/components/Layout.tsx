82e9ac62306c8762f4b25578b2fa4afa125b19af
fceja <cisco_ceja@icloud.com>
2023-08-13 13:19:33 -0700

    update to include logo image in home page only

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,23 @@
-import React from "react"
+import React from "react";
 
-import NavBar from "./NavBar"
+import Logo from "./Logo";
+import { PAGE_PATHS } from "ts/common/Contants";
+import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
-  return(
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
     <div className="page-layout">
+      {window.location.pathname === PAGE_PATHS.HOME && <Logo />}
       <NavBar />
       {children}
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
