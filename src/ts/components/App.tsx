a8f3afe125c13e29dd98aeaada7d2f29282f3ce1
fceja <cisco_ceja@icloud.com>
2023-08-23 20:50:43 -0700

    add lazy loading

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,43 +1,50 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import "../../styles/App.scss"
-import ContactContainer from "ts/pages/contact/ContactContainer";
+import "../../styles/App.scss";
 import HomeContainer from "ts/pages/home/HomeContainer";
 import LayoutContainer from "ts/pages/LayoutContainer";
-import MyGalleryContainer from "ts/pages/myGallery/MyGalleryContainer";
 import { PAGE_PATHS } from "ts/common/Contants";
+
+const MyGalleryContainer = lazy(
+  () => import("ts/pages/myGallery/MyGalleryContainer")
+);
+const ContactContainer = lazy(
+  () => import("ts/pages/contact/ContactContainer")
+);
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate to={PAGE_PATHS.HOME} />} />
-        <Route
-          path={PAGE_PATHS.HOME}
-          element={
-            <LayoutContainer>
-              <HomeContainer />
-            </LayoutContainer>
-          }
-        />
-        <Route
-          path={PAGE_PATHS.MYGALLERY}
-          element={
-            <LayoutContainer>
-              <MyGalleryContainer />
-            </LayoutContainer>
-          }
-        />
-        <Route
-          path={PAGE_PATHS.CONTACT}
-          element={
-            <LayoutContainer>
-              <ContactContainer />
-            </LayoutContainer>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<Navigate to={PAGE_PATHS.HOME} />} />
+          <Route
+            path={PAGE_PATHS.HOME}
+            element={
+              <LayoutContainer>
+                <HomeContainer />
+              </LayoutContainer>
+            }
+          />
+          <Route
+            path={PAGE_PATHS.MYGALLERY}
+            element={
+              <LayoutContainer>
+                <MyGalleryContainer />
+              </LayoutContainer>
+            }
+          />
+          <Route
+            path={PAGE_PATHS.CONTACT}
+            element={
+              <LayoutContainer>
+                <ContactContainer />
+              </LayoutContainer>
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 };
