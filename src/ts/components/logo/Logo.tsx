34a6093c2fa60ee1847243b5379fa520875e4177
fceja <cisco_ceja@icloud.com>
2023-08-29 15:44:43 -0700

    move images to src/assets

--- src/ts/components/logo/Logo.tsx
+++ src/ts/components/logo/Logo.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import "styles/components/logo/Logo.scss";
 import SetScrollVar from "ts/scripts/SetScrollVar";
 
+const logoImage = require("assets/images/logo.png");
+
 export default function Logo() {
   SetScrollVar();
 
   return (
     <div className="img-logo-container">
-      <img className="img-logo-hero" src="images/PopsAndSonsLogo.png" />
+      <img className="img-logo-hero" src={logoImage} />
     </div>
   );
 }
--- src/ts/pages/home/Home.tsx
+++ src/ts/pages/home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import "styles/pages/home/Home.scss";
 
+const mainImage = require("assets/images/main.png");
+
 export default function Home() {
   return (
     <div className="home-img-container">
       <div className="img-gradient-container">
         <div className="gradient" />
-        <img className="img-main" src={"/images/main.png"} alt="electrician" />
+        <img className="img-main" src={mainImage} alt="electrician" />
       </div>
       <div className="img-text">
         <ul>
