d9db5840641288724da48c2448418be3cf355e6a
fceja <cisco_ceja@icloud.com>
2023-11-07 19:32:44 -0800

    rename environment vars

--- src/ts/utils/axiosClient/AxiosClient.ts
+++ src/ts/utils/axiosClient/AxiosClient.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: process.env.REACT_APP_PROFILE_API_URL,
-  timeout: 5000,
+  baseURL: process.env.REACT_APP_EMAILER_API_URL,
+  timeout: process.env.REACT_APP_AXIOS_TIMEOUT
+    ? parseInt(process.env.REACT_APP_AXIOS_TIMEOUT)
+    : 30000,
   headers: {
-    "x-api-key": process.env.REACT_APP_PROFILE_API_KEY,
+    "x-api-key": process.env.REACT_APP_EMAILER_API_KEY,
   },
 });
 
