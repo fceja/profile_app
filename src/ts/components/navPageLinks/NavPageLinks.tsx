60f777ada291a5b81df68dde593095c59230ecfd
fceja <cisco_ceja@icloud.com>
2023-11-14 15:11:54 -0800

    update the btn link styling

--- src/ts/components/navPageLinks/NavPageLinks.tsx
+++ src/ts/components/navPageLinks/NavPageLinks.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import "@scss/components/navPageLinks/NavPageLinks.scss"
+import "@scss/components/navPageLinks/NavPageLinks.scss";
 
 const NavPageLinks: React.FC = () => {
   return (
     <div className="nav-page-links-container">
-      <Link to="/">Home</Link>
-      <Link to="/gallery">Gallery</Link>
-      <Link to="/contact">Contact</Link>
+      <Link className="btn-link" to="/">
+        Home
+      </Link>
+      <Link className="btn-link" to="/gallery">
+        Gallery
+      </Link>
+      <Link className="btn-link" to="/contact">
+        Contact
+      </Link>
     </div>
   );
 };
