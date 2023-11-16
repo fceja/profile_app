bb8ae9329a8d01b4d7dcf4a4dd89ce1cf76c9168
fceja <cisco_ceja@icloud.com>
2023-11-16 15:18:59 -0800

    css adjustments for contactPage

--- src/ts/components/navBar/NavBar.tsx
+++ src/ts/components/navBar/NavBar.tsx
@@ -3,12 +3,11 @@ import React from "react";
 import "@scss/components/navBar/NavBar.scss";
 import HamburgerMenu from "@components/hambugerMenu/HamburgerMenu";
 import { LogoData } from "@data/LogoData";
-import NavPageLinks from "../navPageLinks/NavPageLinks";
+import NavPageLinks from "@components/navPageLinks/NavPageLinks";
 
-// TODO - verify nav bar container sizing is identical across pages / devices
 export default function NavBar() {
   return (
-    <nav className="nav-bar-container" id="id-nav-bar">
+    <nav className="nav-bar-container mt-4" id="id-nav-bar">
       <div className="nav-logo-container">
         <img className="nav-logo" src={LogoData.original} alt={LogoData.alt} />
       </div>
--- src/ts/pages/contactPage/ContactPage.tsx
+++ src/ts/pages/contactPage/ContactPage.tsx
@@ -6,7 +6,7 @@ import ContactFormContainer from "@components/contactForm/ContactFormContainer";
 
 export default function ContactPage() {
   return (
-    <div className="contact-info-form-container pt-5">
+    <div className="contact-info-form-container pt-3">
       <ContactInfo />
       <ContactFormContainer />
     </div>
