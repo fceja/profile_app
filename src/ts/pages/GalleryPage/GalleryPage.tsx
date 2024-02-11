4e7a1ce83c2c6506f227ef3cb60b19c9c5236245
fceja <88167774+fceja@users.noreply.github.com>
2024-02-11 13:02:01 -0800

    remove lazy import, cause issued

--- src/ts/pages/GalleryPage/GalleryPage.tsx
+++ src/ts/pages/GalleryPage/GalleryPage.tsx
@@ -1,23 +1,24 @@
-import React, { lazy, Suspense } from "react";
+import React, { Suspense } from "react";
 
 import "@scss/pages/galleryPage/GalleryPage.scss";
 import { GalleryImageData } from "@data/GalleryData";
 import Loading from "@common/components/Loading"
 
-const ImageGallery = lazy(() => import("react-image-gallery"))
-
+// TODO - can this be lazily loaded? initially threw error.
+import ImageGallery from "react-image-gallery";
 
 // TODO - will need to prepare proper aspect ratio for images, so that the
 // size appears identical in the slider
-const images = GalleryImageData;
 
 const MyImageGallery: React.FC = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <section className="app-react-image-gallery mt-3 mb-3">
-        <ImageGallery items={images} />
-      </section>
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading />}>
+        <section className="app-react-image-gallery mt-3 mb-3">
+          <ImageGallery items={GalleryImageData} />
+        </section>
+      </Suspense>
+    </>
   );
 };
 
